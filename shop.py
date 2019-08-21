64ca680c7258ff83a04d0423ded48650158b0932
samirahisham <engineersamiraawad@gmail.com>
2019-08-21 11:58:42 +0300

    fixed

--- shop.py
+++ shop.py
@@ -21,7 +21,7 @@ def print_menu():
     Print the items in the menu dictionary.
     """
     print("Our menu:")
-    for item in cupcake_menu:
+    for item in menu:
         print("- \"%s\" (KD %s)" % (item, menu[item]))
 
 
@@ -33,7 +33,7 @@ def print_originals():
     """
     print("Our original flavor cupcakes (KD %s each):" % original_price)
     for item in original_flavors:
-    print("- \"%s\"" % item)
+     print("- \"%s\"" % item)
 
 
 def print_signatures():
@@ -65,7 +65,7 @@ def get_order():
     """
     order_list = []
     order = input("What's your order? (Enter the exact spelling of the item you want. Type 'Exit' to end your order.)\n")
-    while order.lowercase() != "exit":
+    while order.lower() != "exit":
         if is_valid_order(order):
             order_list.append(order)
         order = input()
